176e130368190f3558236039ca011d9f455e9c64
Miká Juznik <77588816+miju04@users.noreply.github.com>
2026-09-01 11:53:28 +0200

    feat(tmux): initialize project workspaces

--- tmux/dot-config/tmux/tmux-sessionizer.sh
+++ tmux/dot-config/tmux/tmux-sessionizer.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-	selected=$1
+  selected=$1
 else
-	directories=(
-		"$HOME/dev/"
-		"$HOME/dotfiles/"
-	)
+  directories=(
+    "$HOME/dev/"
+    "$HOME/dotfiles/"
+  )
 
-	if [[ -d "$HOME/OneDrive/lebk/" ]]; then
-		directories+=("$HOME/OneDrive/lebk/*jahr/stdm/programme")
-	fi
+  selected=$(fd -HI -t d '^\.git$' "${directories[@]}" --max-depth 5 --exclude 'dot*' \
+    --exec dirname {} | while IFS= read -r directory; do
+      printf '%s\n' "${directory#"$HOME"/}"
+    done | fzf)
 
-	selected=$(fd -HI -t d '^\.git$' "${directories[@]}" --max-depth 5 --exclude 'dot*' \
-		--exec dirname {} | fzf)
+  if [[ -n $selected ]]; then
+    selected="$HOME/$selected"
+  fi
 fi
 
 if [[ -z $selected ]]; then
-	exit 0
+  exit 0
 fi
 
 selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s $selected_name -c $selected
-	exit 0
-fi
-
-if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	tmux new-session -ds $selected_name -c $selected
+if ! tmux has-session -t="$selected_name" 2>/dev/null; then
+  tmux new-session -ds "$selected_name" -n nvim -c "$selected" nvim
+  tmux new-window -t "$selected_name" -n opencode -c "$selected" opencode
+  tmux new-window -t "$selected_name" -c "$selected"
+  tmux select-window -t "$selected_name:zsh"
 fi
 
 if [[ -z $TMUX ]]; then
-	tmux attach -t $selected_name
+  tmux attach -t "$selected_name"
 else
-	tmux switch-client -t $selected_name
+  tmux switch-client -t "$selected_name"
 fi
